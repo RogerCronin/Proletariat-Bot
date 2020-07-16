b4eb7cd7cd3a31ec10b4ab5928607a1b401ebb30
RogerCronin <57421158+RogerCronin@users.noreply.github.com>
2020-07-16 11:07:15 -0400

    Added GitHub links

--- commands/general/link.js
+++ commands/general/link.js
@@ -6,6 +6,6 @@ module.exports = {
 	name: "link",
 	title: "link",
 	description: "Nab an invite link to spread Communism all across Discord!",
-	execute: (message, args) => message.channel.send("Have fun and stay safe\nhttps://discord.com/api/oauth2/authorize?client_id=513455833703645184&permissions=388160&scope=bot\n\nOh and here's a link to the code\nGITHUB_LINK"),
+	execute: (message, args) => message.channel.send("Have fun and stay safe\nhttps://discord.com/api/oauth2/authorize?client_id=513455833703645184&permissions=388160&scope=bot\n\nOh and here's a link to the code\nhttps://github.com/RogerCronin/Proletariat-Bot"),
 	checkSyntax: (message, args) => args[1] ? "More arguments than expected" : true
 }
