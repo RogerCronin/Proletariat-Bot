ff033fcb187b945162c99952443b1ff2bc9e62bd
Roger <57421158+RogerCronin@users.noreply.github.com>
2020-10-14 18:38:03 -0400

    ba

--- commands/admin/update.js
+++ commands/admin/update.js
@@ -9,15 +9,22 @@ module.exports = new banner.Command({
 	description: "Updates the bot to the latest version.",
 	category: "admin",
 	execute: async function(message, args) {
+    console.log(1)
     if(message.channel.id != "765996274721751040") return message.channel.send(this.errorMessage("This command can only be executed in the bot-logs channel.", "update:0"))
+    console.log(2)
     const msg_1 = await message.channel.send("❔ Pull latest version from GitHub")
+    console.log(3)
     console.log(execSync("git pull"))
+    console.log(4)
     await msg_1.edit("✔️ Pull latest version from GitHub")
+    console.log(5)
     const msg_2 = await message.channel.send("❔ Restart bot")
+    console.log(6)
     //console.log(execSync("pm2 restart Proletariat-Bot"))
 	},
 	checkSyntax: function(message, args) {
-    if(message.user.id != "316947582670602240") return "You don't have proper credentials!"
+    console.log("check")
+    if(message.author.id != "316947582670602240") return "You don't have proper credentials!"
     return true
   }
 })
